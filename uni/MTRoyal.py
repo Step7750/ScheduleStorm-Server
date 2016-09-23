9e2b48fbc0c152036df8294d715d403654024cc9
Step7750 <Step7750@gmail.com>
2016-09-22 19:58:46 -0600

    Added MT logging in, term and subject retrieval

--- uni/MTRoyal.py
+++ uni/MTRoyal.py
@@ -8,17 +8,23 @@ This file is a resource for Schedule Storm - https://github.com/Step7750/Schedul
 
 import threading
 import pymongo
+from bs4 import BeautifulSoup
 import time
 import logging
+import requests
+from datetime import datetime
+import json
 
 
 log = logging.getLogger("MTRoyal")
+verifyRequests = False
 
 
 class MTRoyal(threading.Thread):
     def __init__(self, settings):
         threading.Thread.__init__(self)
         self.settings = settings
+        self.loginSession = requests.session()
         self.db = pymongo.MongoClient().ScheduleStorm
 
     def getTerms(self):
@@ -67,6 +73,117 @@ class MTRoyal(threading.Thread):
         # Send over a list of all the professors with a RMP rating in the list
         return {"classes": {}, "rmp": {}}
 
+    def login(self):
+        """
+        Logs into Mount Royal
+
+        :return: **boolean** True if it logged in successfully, False if not
+        """
+
+        logindata = {
+            "sid": self.settings["userid"],
+            "PIN": self.settings["pin"]
+        }
+
+        loginpage = self.loginSession.get("https://mruweb.mymru.ca/prod/bwskfreg.P_AltPin", verify=False)
+
+        if loginpage.status_code == requests.codes.ok:
+            response = self.loginSession.post("https://mruweb.mymru.ca/prod/twbkwbis.P_ValLogin", data=logindata,
+                                          verify=verifyRequests)
+            if response.status_code == requests.codes.ok:
+                return True
+            else:
+                return False
+
+        else:
+            return False
+
+    def getTerms(self):
+        """
+        Retrieves and parses the terms list
+
+        :return: **list**
+        """
+        termpage = self.loginSession.get("https://mruweb.mymru.ca/prod/bwskfcls.p_sel_crse_search")
+
+        if termpage.status_code == requests.codes.ok:
+            # parse the contents
+            soup = BeautifulSoup(termpage.text, "lxml")
+
+            response_dict = {}
+
+            for termoption in  soup.find("select", {"name": "p_term"}).findAll("option"):
+                log.debug("Processing " + termoption['value'])
+
+                if len(termoption['value']) > 1:
+                    termtext = termoption.text
+
+                    # We want this year or next year in the text (don't want old terms)
+                    thisyear = datetime.now().year
+
+                    if str(thisyear) in termtext or str(thisyear+1) in termtext:
+                        log.debug(termtext + " is within this year or the next")
+
+                        # We dont want to present the terms that have "View Only" since users cant register in them
+                        # anyways
+                        if "view only" not in termtext.lower():
+                            # add it to the dict
+                            response_dict[termoption['value']] = termtext.strip()
+
+            return response_dict
+
+        else:
+            return False
+
+    def getSubjectsForTerm(self, termid):
+        """
+        Returns the subjects for the given term
+
+        :param termid: **int/string** Term ID to get subjects for
+        :return: **dict** Subjects in the specified term
+        """
+
+        advancedsearch = self.loginSession.post("https://mruweb.mymru.ca/prod/bwskfcls.P_GetCrse",
+                                             data="rsts=dummy"
+                                                  "&crn=dummy"
+                                                  "&term_in=" + str(termid) +
+                                                  "&sel_subj=dummy"
+                                                  "&sel_day=dummy"
+                                                  "&sel_schd=dummy"
+                                                  "&sel_insm=dummy"
+                                                  "&sel_camp=dummy"
+                                                  "&sel_levl=dummy"
+                                                  "&sel_sess=dummy"
+                                                  "&sel_instr=dummy"
+                                                  "&sel_ptrm=dummy"
+                                                  "&sel_attr=dummy"
+                                                  "&sel_crse="
+                                                  "&sel_title="
+                                                  "&sel_from_cred="
+                                                  "&sel_to_cred="
+                                                  "&sel_ptrm=%25"
+                                                  "&begin_hh=0"
+                                                  "&begin_mi=0"
+                                                  "&end_hh=0"
+                                                  "&end_mi=0"
+                                                  "&begin_ap=x"
+                                                  "&end_ap=y"
+                                                  "&path=1"
+                                                  "&SUB_BTN=Advanced+Search",
+                                             verify=verifyRequests)
+
+        if advancedsearch.status_code == requests.codes.ok:
+            subjects = {}
+
+            soup = BeautifulSoup(advancedsearch.text, "lxml")
+
+            for subject in soup.find("select", {"name": "sel_subj"}).findAll("option"):
+                subjects[subject['value']] = subject.text.strip()
+
+            return subjects
+        else:
+            return False
+
     def run(self):
         """
         Scraping thread that obtains updated course info
@@ -78,6 +195,16 @@ class MTRoyal(threading.Thread):
             while True:
                 try:
                     log.info("Scraping now")
+                    if self.login():
+                        terms = self.getTerms()
+
+                        log.info(terms)
+
+                        if terms:
+                            for term in terms:
+                                termsubjects = self.getSubjectsForTerm(term)
+                                log.info(termsubjects)
+                                
                 except Exception as e:
                     log.critical("There was an critical exception | " + str(e))
 
