8b9f034624bbe2b5232ff918c19af19d755085d6
Step7750 <Step7750@gmail.com>
2016-10-30 13:05:47 -0600

    Example: Updated to inherit University class

--- uni/Example.py
+++ uni/Example.py
@@ -6,66 +6,12 @@ Under MIT License - https://github.com/Step7750/ScheduleStorm/blob/master/LICENS
 This file is a resource for Schedule Storm - https://github.com/Step7750/ScheduleStorm
 """
 
-import threading
-import pymongo
-import time
-import logging
+from .University import University
 
 
-log = logging.getLogger("Example")
-
-
-class Example(threading.Thread):
+class Example(University):
     def __init__(self, settings):
-        threading.Thread.__init__(self)
-        self.settings = settings
-        self.db = pymongo.MongoClient().ScheduleStorm
-
-    def getTerms(self):
-        """
-        API Handler
-
-        Returns the distinct terms in the database, along with their name and id
-
-        :return: **dict** Keys are the ids, values are the proper names
-        """
-        termlist = self.db.ExampleCourseList.distinct("term")
-        responsedict = {}
-
-        for term in termlist:
-            responsedict[str(term)] = self.termIDToName(term)
-
-        return responsedict
-
-    def getLocations(self):
-        """
-        API Handler
-
-        Returns a list of all locations at UCalgary
-
-        :return: **list** Contains 1D with the possible locations
-        """
-        locations = self.db.ExampleCourseList.distinct("location")
-        response = []
-
-        for location in locations:
-            if location != "":
-                response.append(location)
-
-        return response
-
-    def getSubjectListAll(self, term):
-        """
-        API Handler
-
-        Returns all data for a given term (classes, descriptions and RMP)
-
-        :param term: **string/int** ID of the term
-        :return: **dict** All data for the term
-        """
-
-        # Send over a list of all the professors with a RMP rating in the list
-        return {"classes": {}, "rmp": {}}
+        super().__init__(settings)
 
     def run(self):
         """
@@ -73,15 +19,4 @@ class Example(threading.Thread):
 
         :return:
         """
-
-        if self.settings["scrape"]:
-            while True:
-                try:
-                    pass
-                except Exception as e:
-                    log.critical("There was an critical exception | " + str(e))
-
-                # Sleep for the specified interval
-                time.sleep(self.settings["scrapeinterval"])
-        else:
-            log.info("Scraping is disabled")
+        self.log.info("Obtain course info here!")
