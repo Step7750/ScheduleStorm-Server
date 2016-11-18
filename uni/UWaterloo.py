ab86f2b744c5d7b281f21c207711f56b28d6b237
Ceegan Hale <ceeganhale@gmail.com>
2016-11-17 21:34:39 -0700

    UWaterloo: Class and Course Description scraper

--- uni/UWaterloo.py
+++ uni/UWaterloo.py
@@ -11,6 +11,7 @@ from uwaterlooapi import UWaterlooAPI
 import logging
 import time
 import pymongo
+from datetime import datetime
 
 log = logging.getLogger("UWaterloo")
 
@@ -21,23 +22,70 @@ class UWaterloo(University):
         self.settings = settings
         self.db = pymongo.MongoClient().ScheduleStorm
 
+    def scrapeCourseList(self, uw, term):
+        courseList = []
+        for course in uw.term_courses(term):
+            courseInfo = uw.term_course_schedule(term, course['subject'], course['catalog_number'])
+            if len(courseInfo) != 0:
+                courseDict = {'coursenum': course['catalog_number'], 'subject': course['subject'], 'term': term,
+                              'id': courseInfo[0]['class_number'], 'group': course['class_number'],
+                              'type': courseInfo[0]['section'][:3], 'location': courseInfo[0]['campus'],
+                              'status': str(courseInfo[0]['enrollment_total'])+'/' +
+                                        str(courseInfo[0]['enrollment_capacity'])}
+
+                for date in courseInfo[0]['classes']:
+                    courseDict['rooms'] = [date['location']['building'], date['location']['room']]
+
+                    if date['date']['start_time']:
+                        course_start_time = datetime.strptime(date['date']['start_time'], '%H:%M')
+                        course_end_time = datetime.strptime(date['date']['end_time'], '%H:%M')
+                        courseDict['times'] = [date['date']['weekdays'] + " " + course_start_time.strftime('%I:%M%p') +
+                                               ' - ' + course_end_time.strftime('%I:%M%p')]
+                    else:
+                        courseDict['times'] = ['N/A']
+
+                    if len(date['instructors']) == 0:
+                        courseDict['instructor'] = ['N/A']
+                    else:
+                        courseDict['instructor'] = date['instructors']
+                    courseList.append(courseDict)
+            else:
+                courseDict = {'coursenum': course['catalog_number'], 'subject': course['subject'], 'term': term,
+                              'id': 'N/A', 'rooms': ['N/A'], 'type': 'N/A', 'location': 'N/A', 'status': 'N/A',
+                              'instructor': ['N/A'], 'group': course['catalog_number']}
+                courseList.append(courseDict)
+
+            courseDesc = uw.course(course['subject'], course['catalog_number'])
+            courseDict = {'coursenum': course['catalog_number'], 'subject': course['subject'],
+                          'name': courseInfo[0]['title'], 'desc': courseDesc[0]['description'],
+                          'units': courseInfo[0]['units'], 'prereq': courseDesc[0]['prerequisites'],
+                          'coreq': courseDesc[0]['corequisites'], 'antireq': courseDesc[0]['antirequisites'],
+                          'notes': courseInfo[0]['note']}
+
+            #self.updateCourseDesc(courseDict)
+        #self.updateClasses(courseList)
+
     def scrapeTerms(self, uw):
-        print(uw.terms())
         log.info("SCraping terms")
         termDictList = []
         terms = uw.terms()
         termList = terms['listings']
+
         for term in termList:
             for x in range(len(termList[term])):
+
                 if termList[term][x]['id'] == terms['previous_term']:
                     termDict = {'id': terms['previous_term'], 'name': termList[term][x]['name']}
                     termDictList.append(termDict)
+
                 elif termList[term][x]['id'] == terms['current_term']:
                     termDict = {'id': terms['current_term'], 'name': termList[term][x]['name']}
                     termDictList.append(termDict)
+
                 elif termList[term][x]['id'] == terms['next_term']:
                     termDict = {'id': terms['next_term'], 'name': termList[term][x]['name']}
                     termDictList.append(termDict)
+
         self.updateTerms(termDictList)
         log.info('Finished scraping terms')
 
@@ -70,6 +118,9 @@ class UWaterloo(University):
                     self.scrapeTerms(uw)
                     terms = self.getTerms()
 
+                    for term in terms:
+                        log.info('Obtaining ' + terms[term] + ' course data with id ' + term)
+                        self.scrapeCourseList(uw, term)
                     log.info('Finished scraping for UWaterloo data')
                 except Exception as e:
                     log.info("There was a critical exception | " + str(e))
