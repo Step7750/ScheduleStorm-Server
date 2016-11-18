ee8f6f3bf1f6113b45b0410fbc32ed48d9c69251
Ceegan Hale <ceeganhale@gmail.com>
2016-11-18 14:04:32 -0700

    UWaterloo: Fixes duplicate classes issue

--- uni/UWaterloo.py
+++ uni/UWaterloo.py
@@ -22,50 +22,52 @@ class UWaterloo(University):
         self.settings = settings
         self.db = pymongo.MongoClient().ScheduleStorm
 
-    def scrapeCourseList(self, uw, term):
+    def scrapeCourseList(self, uw, term, subjectList):
         courseList = []
-        for subject in uw.term_courses(term):
+
+        for subject in subjectList:
             courseInfo = uw.term_subject_schedule(term, subject['subject'])
+            #print(subject)
             for course in courseInfo:
-                if len(courseInfo) != 0:
-                    courseDict = {'coursenum': subject['catalog_number'], 'subject': subject['subject'], 'term': term,
-                                  'id': course['class_number'], 'group': course['class_number'],
-                                  'type': course['section'][:3], 'location': course['campus'],
-                                  'status': str(course['enrollment_total'])+'/' + str(course['enrollment_capacity'])}
+                if subject['subject'] == 'ACC':
+                    print(course)
+                courseDict = {'coursenum': course['catalog_number'], 'subject': subject['subject'], 'term': term,
+                              'id': course['class_number'], 'group': course['class_number'],
+                              'type': course['section'][:3], 'location': course['campus'],
+                              'status': str(course['enrollment_total'])+'/' + str(course['enrollment_capacity'])}
 
-                    for date in courseInfo[0]['classes']:
-                        courseDict['rooms'] = [date['location']['building'], date['location']['room']]
+                for date in course['classes']:
+                    courseDict['rooms'] = [date['location']['building'], date['location']['room']]
+                    if date['date']['start_time']:
+                        course_start_time = datetime.strptime(date['date']['start_time'], '%H:%M')
+                        course_end_time = datetime.strptime(date['date']['end_time'], '%H:%M')
+                        courseDict['times'] = [date['date']['weekdays'] + " " + course_start_time.strftime('%I:%M%p') +
+                                               ' - ' + course_end_time.strftime('%I:%M%p')]
+                    else:
+                        courseDict['times'] = ['N/A']
 
-                        if date['date']['start_time']:
-                            course_start_time = datetime.strptime(date['date']['start_time'], '%H:%M')
-                            course_end_time = datetime.strptime(date['date']['end_time'], '%H:%M')
-                            courseDict['times'] = [date['date']['weekdays'] + " " + course_start_time.strftime('%I:%M%p') +
-                                                   ' - ' + course_end_time.strftime('%I:%M%p')]
-                        else:
-                            courseDict['times'] = ['N/A']
-
-                        if len(date['instructors']) == 0:
-                            courseDict['instructor'] = ['N/A']
-                        else:
-                            courseDict['instructor'] = date['instructors']
-                        courseList.append(courseDict)
-                else:
-                    courseDict = {'coursenum': subject['catalog_number'], 'subject': subject['subject'], 'term': term,
-                                  'id': 'N/A', 'rooms': ['N/A'], 'type': 'N/A', 'location': 'N/A', 'status': 'N/A',
-                                  'instructor': ['N/A'], 'group': subject['catalog_number']}
+                    if date['instructors']:
+                        courseDict['instructor'] = date['instructors']
+                    else:
+                        courseDict['instructor'] = ['N/A']
                     courseList.append(courseDict)
-                print(courseDict)
-                courseDesc = uw.course(subject['subject'], subject['catalog_number'])
+                '''if len(course['classes']) > 1:
+                    print('ayy', courseDict)
+                    time.sleep(20)'''
+                courseDesc = uw.course(subject['subject'], course['catalog_number'])
                 if len(courseDesc) != 0:
-                    courseDict = {'coursenum': subject['catalog_number'], 'subject': subject['subject'],
+                    courseDict = {'coursenum': course['catalog_number'], 'subject': subject['subject'],
                                   'name': course['title'], 'desc': courseDesc['description'],
                                   'units': course['units'], 'prereq': courseDesc['prerequisites'],
                                   'coreq': courseDesc['corequisites'], 'antireq': courseDesc['antirequisites'],
                                   'notes': course['note']}
                 else:
-                    courseDict = {'coursenum': subject['catalog_number'], 'subject': subject['subject']}
-                self.updateCourseDesc(courseDict)
-        self.updateClasses(courseList)
+                    courseDict = {'coursenum': course['catalog_number'], 'subject': subject['subject']}
+                #self.updateCourseDesc(courseDict)
+            #print(len(courseInfo))
+            print('lmao', courseList)
+            #time.sleep(30)
+        #self.updateClasses(courseList)
 
     def scrapeTerms(self, uw):
         log.info("SCraping terms")
@@ -94,14 +96,17 @@ class UWaterloo(University):
     def updateFaculties(self, uw):
         log.info("Getting faculty list")
         faculties = uw.group_codes()
-
+        subjectList = []
         for subject in uw.subject_codes():
             subjectDict = {'subject': subject['subject'], 'faculty': '', 'name': subject['description']}
+
             for faculty in faculties:
                 if subject['group'] == faculty['group_code']:
                     subjectDict['faculty'] = faculty['group_full_name']
-            self.updateSubject(subjectDict)
+            subjectList.append(subjectDict)
+        self.updateSubjects(subjectList)
         log.info('Finished updating faculties')
+        return subjectList
 
     def run(self):
         """
@@ -116,13 +121,13 @@ class UWaterloo(University):
                     uw = UWaterlooAPI(api_key=self.settings['api_key'])
                     #print(dir(uw))
 
-                    self.updateFaculties(uw)
-                    self.scrapeTerms(uw)
+                    subjectList = self.updateFaculties(uw)
+                    #self.scrapeTerms(uw)
                     terms = self.getTerms()
 
                     for term in terms:
                         log.info('Obtaining ' + terms[term] + ' course data with id ' + term)
-                        self.scrapeCourseList(uw, term)
+                        self.scrapeCourseList(uw, term, subjectList)
 
                     log.info('Finished scraping for UWaterloo data')
                 except Exception as e:
