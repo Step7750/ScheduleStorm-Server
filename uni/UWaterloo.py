0ce42f59995435da612a274bd82934e0499214bf
Ceegan Hale <ceeganhale@gmail.com>
2016-11-24 17:51:26 -0700

    UWaterloo: Adds section key to courseDict for displaying instead of group

--- uni/UWaterloo.py
+++ uni/UWaterloo.py
@@ -137,7 +137,8 @@ class UWaterloo(University):
                                   'id': course['class_number'], 'type': course['section'][:3], 'group': [],
                                   'location': course['campus'], 'curEnroll': course['enrollment_capacity'],
                                   'rooms': [date['location']['building'], date['location']['room']],
-                                  'capEnroll': course['enrollment_total'], 'capwaitEnroll': course['waiting_capacity']}
+                                  'capEnroll': course['enrollment_total'], 'capwaitEnroll': course['waiting_capacity'],
+                                  'section': course['section'][4:]}
 
                     # Checks if class is open, closed, or has a waiting list
                     if course['enrollment_capacity'] != 0 and course['enrollment_total']/course['enrollment_capacity'] >= 1:
